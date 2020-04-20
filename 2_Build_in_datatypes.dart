fbf9bbfc9a712e04ba7773b4cf3f70550fddb4de
TUSHAR <53440987+akhiltushar@users.noreply.github.com>
2020-04-20 21:53:28 +0530

    Add files via upload

---
+++ 2_Build_in_datatypes.dart
@@ -0,0 +1,29 @@
+
+void main() {
+
+  // Numbers: int
+
+  int marks = 75;
+  var marks = 75; // It is deduce as integer automatically by compiler.
+  int hexvalue = 1513FEE;
+
+
+  // Numbers: Double
+  double percentage = 87.6;
+  var percentage = 87.9;
+  double exponents = 1.42e5;
+
+  // Strings
+  String name = "TUSHAR";
+  var company = "Google";
+
+  // Boolean
+  bool istrue = true;
+  var isfalse = true;
+
+  print(percentage);
+  print(exponents);
+
+  // NOTE: All data type in Dart are Object.
+  // Therefore, their initial value is by default 'null'.
+}
---
+++ 3_String_&_String_Interpolation.dart
@@ -0,0 +1,34 @@
+void main() {
+
+   // Literals
+  
+  var istrue = true;
+  int x = 2;
+  "Akhil";
+  4.5;
+
+  // Various ways to define String Literals in Dart
+
+  String s1 = 'Single';
+  String s2 = "Double";
+  String s3 = 'It\'s easy';
+	String s4 = "It's easy";
+
+	String s5 = 'This is going to be a very long String. '
+			'This is just a sample String demo in Dart Programming Language';
+
+  // String Interpolation : Use ["My name is $name"] instead of ["My name is " + name]
+
+	String name = "Ram";
+
+	print("My name is $name");
+	print("The number of characters in String Ram is ${name.length}");
+
+
+	int l = 20;
+	int b = 10;
+
+	print("The sum of $l and $b is ${l + b}");
+	print("The area of rectangle with length $l and breadth $b is ${l * b}");
+  
+}
