7dddd81c46b96b766579c1ac834604fd4dc42bb7
TUSHAR <53440987+akhiltushar@users.noreply.github.com>
2020-05-01 12:43:10 +0530

    Add files via upload

---
+++ 4_Constants.dart
@@ -0,0 +1,19 @@
+void main()
+ {
+  // Final
+  final cityName = 'Delhi';
+  //     name = 'Akhil';     // Throws an error
+
+  final String countryName = 'India';
+
+  // const
+  const PI = 3.14;
+  const double gravity = 9.8;
+
+}
+
+class Circle {
+
+  final color = 'Blue';
+  static const PI = 3.14;
+}
